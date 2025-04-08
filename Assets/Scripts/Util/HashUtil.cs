96847184a34b226b0e4902b59630205fb6625709
신관규 <soundbar91@gmail.com>
2025-04-08 23:11:45 +0900

    feat: 해쉬 유틸리티 클래스 추가

---
+++ Assets/Scripts/Util/HashUtil.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public static class HashUtil
+{
+    public static string ComputeSHA256(string rawData)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in bytes)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
