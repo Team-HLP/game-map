1b1b5c2ccb2ed94a9e3dd1102750752a68edd2a7
신관규 <soundbar91@gmail.com>
2025-05-15 15:52:29 +0900

    feat: 두더지 게임 결과 연결

---
+++ Assets/3.Scripts/HistoryEntryUICopy.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HistoryEntryUICopy : MonoBehaviour
+{
+    public Text createdAtText;
+    public Text resultText;
+    public Text scoreText;
+
+    private ResultUIManager resultUIManager;
+
+    private string result;
+    private int score;
+    private int hp;
+    private int meteorCount;
+    private string createdAt;
+
+    public void Initialize(string result, int score, int hp, int meteorCount, string created_at, ResultUIManager manager)
+    {
+        createdAtText.text = created_at;
+        resultText.text = result;
+        scoreText.text = $"{score}��";
+
+        this.createdAt = created_at;
+        this.result = result;
+        this.score = score;
+        this.hp = hp;
+        this.meteorCount = meteorCount;
+
+        resultUIManager = manager;
+
+        GetComponent<Button>().onClick.AddListener(OnClickEntry);
+    }
+
+    void OnClickEntry()
+    {
+        resultUIManager.DisplayDetails(createdAt, result, score, meteorCount, hp);
+    }
+}
--- Assets/3.Scripts/ResultUIManager.cs
+++ Assets/3.Scripts/ResultUIManager.cs
@@ -21,10 +21,17 @@ public class ResultUIManager : MonoBehaviour
     public Transform contentParent;
     public GameObject detailPanel;
 
+    [Header("두더지 히스토리 리스트")]
+    public GameObject moleHistoryEntryPrefab;
+    public Transform moleContentParent;
+    public GameObject moleDetailPanel;
+
     void Start()
     {
         if (detailPanel != null)
             detailPanel.SetActive(false);
+        if (moleDetailPanel != null)
+            moleDetailPanel.SetActive(false);
 
         StartCoroutine(LoadGameResultsFromServer());
     }
@@ -35,7 +42,7 @@ public class ResultUIManager : MonoBehaviour
         UnityWebRequest request = UnityWebRequest.Get(url);
 
         string accessToken = PlayerPrefs.GetString("access_token", "");
-        request.SetRequestHeader("Authorization", "Bearer " + accessToken);
+        request.SetRequestHeader("Authorization", "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6MzksImV4cCI6MTc0NzI5NTk2MX0.h6Xac4xo0Ule8fw8zcQoNb5-zbYNNPAQEbjugeoP7GD4gTMvFlQqDnjHxU1gi8VUwP7mXw7kE3rvxi9rtyiCog");
 
         yield return request.SendWebRequest();
 
@@ -70,12 +77,59 @@ public class ResultUIManager : MonoBehaviour
             Debug.LogError("서버에서 결과 불러오기 실패: " + request.error);
             Debug.LogError("서버 응답:\n" + request.downloadHandler.text);
         }
+
+        url = Apiconfig.url + "/games?gameCategory=CATCH_MOLE";
+        request = UnityWebRequest.Get(url);
+
+        accessToken = PlayerPrefs.GetString("access_token", "");
+        request.SetRequestHeader("Authorization", "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6MzksImV4cCI6MTc0NzI5NTk2MX0.h6Xac4xo0Ule8fw8zcQoNb5-zbYNNPAQEbjugeoP7GD4gTMvFlQqDnjHxU1gi8VUwP7mXw7kE3rvxi9rtyiCog");
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string json = request.downloadHandler.text;
+            Debug.Log("서버에서 받은 결과:\n" + json);
+
+            List<GameResultResponse> serverResults = JsonConvert.DeserializeObject<List<GameResultResponse>>(json);
+
+            foreach (var result in serverResults)
+            {
+                string resultStr = result.result == "성공" ? "성공" : "실패";
+
+                DateTime createdTime;
+                string displayTime;
+
+                if (DateTime.TryParseExact(result.created_at, "yyyy.MM.dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out createdTime))
+                {
+                    displayTime = createdTime.ToString("yyyy.MM.dd HH:mm");
+                }
+                else
+                {
+                    displayTime = result.created_at;
+                }
+
+                AddMoleHistoryEntry(resultStr, result.score, result.hp, result.meteorite_broken_count, displayTime);
+            }
+        }
+        else
+        {
+            Debug.LogError("서버에서 결과 불러오기 실패: " + request.error);
+            Debug.LogError("서버 응답:\n" + request.downloadHandler.text);
+        }
     }
 
     public void AddHistoryEntry(string result, int score, int hp, int meteorCount, string created_at)
     {
         GameObject entryGO = Instantiate(historyEntryPrefab, contentParent);
         HistoryEntryUI entryUI = entryGO.GetComponent<HistoryEntryUI>();
+        entryUI.Initialize(result, score, hp, meteorCount, created_at, this);
+    }
+
+    public void AddMoleHistoryEntry(string result, int score, int hp, int meteorCount, string created_at)
+    {
+        GameObject entryGO = Instantiate(moleHistoryEntryPrefab, moleContentParent);
+        HistoryEntryUICopy entryUI = entryGO.GetComponent<HistoryEntryUICopy>();
         entryUI.Initialize(result, score, hp, meteorCount, created_at, this);
     }
 
