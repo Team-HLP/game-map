c0e099c6cb6659c102a58669f20c95c42d5a3d4f
랩실 <soundbar91@gmail.com>
2025-05-25 20:31:12 +0900

    fix: 메모리 누수 해결

--- Assets/3.Scripts/GameManager.cs
+++ Assets/3.Scripts/GameManager.cs
@@ -270,23 +270,31 @@ public class GameManager : MonoBehaviour
         int meteorite_prefab_count = PlayerPrefs.GetInt("meteorite_prefab_count");
         int fuel_prefab_count = PlayerPrefs.GetInt("fuel_prefab_count");
 
-        string jsonBody = JsonUtility.ToJson(new GameResultRequest(meteorite_prefab_count, fuel_prefab_count, result, score, hp, meteorite_broken_count));
-        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        string jsonBody = JsonUtility.ToJson(
+            new GameResultRequest(meteorite_prefab_count, fuel_prefab_count, result, score, hp, meteorite_broken_count)
+        );
 
         byte[] gmaeResult = Encoding.UTF8.GetBytes(jsonBody);
         byte[] eegBytes = File.ReadAllBytes(eegFilePath);
         byte[] eyeBytes = File.ReadAllBytes(eyeFilePath);
         byte[] behaviorBytes = File.ReadAllBytes(behaviorFilePath);
 
-        formData.Add(new MultipartFormFileSection("request", gmaeResult, "request.json", "application/json"));
-        formData.Add(new MultipartFormFileSection("eeg_data_file", eegBytes, "eeg_data.json", "application/json"));
-        formData.Add(new MultipartFormFileSection("eye_data_file", eyeBytes, "eye_data.json", "application/json"));
-        formData.Add(new MultipartFormFileSection("behavior_file", behaviorBytes, "behavior_data.json", "application/json"));
+        List<IMultipartFormSection> formData = new()
+        {
+            new MultipartFormFileSection("request", gmaeResult, "request.json", "application/json"),
+            new MultipartFormFileSection("eeg_data_file", eegBytes, "eeg_data.json", "application/json"),
+            new MultipartFormFileSection("eye_data_file", eyeBytes, "eye_data.json", "application/json"),
+            new MultipartFormFileSection("behavior_file", behaviorBytes, "behavior_data.json", "application/json")
+        };
 
-        UnityWebRequest request = UnityWebRequest.Post(Apiconfig.url + "/game/meteorite", formData);
-        request.SetRequestHeader("Authorization", "Bearer " + PlayerPrefs.GetString("access_token", ""));
-
-        yield return request.SendWebRequest();
+        using (UnityWebRequest request = UnityWebRequest.Post(Apiconfig.url + "/game/meteorite", formData))
+        {
+            request.SetRequestHeader("Authorization", "Bearer " + PlayerPrefs.GetString("access_token", ""));
+            
+            yield return request.SendWebRequest();
+            request.uploadHandler?.Dispose();
+            request.downloadHandler?.Dispose();
+        }
     }
 
     public void ImmeditelyBioDataSave()
